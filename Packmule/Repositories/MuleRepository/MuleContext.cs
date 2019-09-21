4c96aedd3071c55f4310d7b5c7cfeab5093e205a
Curtis Zorn <curtis.zorn@slalom.com>
2019-09-21 12:30:27 -0500

    * patch

--- Packmule/Repositories/MuleRepository/MuleContext.cs
+++ Packmule/Repositories/MuleRepository/MuleContext.cs
@@ -15,15 +15,6 @@ namespace Packmule.Repositories.MuleRepository
 
         public virtual DbSet<Notifications> Notifications { get; set; }
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:mulebarn.database.windows.net,1433;Initial Catalog=mule;Persist Security Info=False;User ID=***REMOVED***;Password=***REMOVED***;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
-            }
-        }
-
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
